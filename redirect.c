e94aa1ea96d1644f6959d11254f89c5c66b1486d
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2014-10-02 01:32:37 +0200

    dup2() closes newfd anyway

--- redirect.c
+++ redirect.c
@@ -4,8 +4,6 @@
 
 void pipes_redirect_fd(int oldfd, int newfd, const char *msg) {
 	if (oldfd > -1 && oldfd != newfd) {
-		close(newfd);
-
 		if (dup2(oldfd, newfd) == -1) {
 			perror(msg);
 			exit(EXIT_FAILURE);
